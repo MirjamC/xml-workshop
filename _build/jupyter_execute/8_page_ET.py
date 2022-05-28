abcf5d4c565c3c4cd5596fa862554fd7cf69eac7
Mirjam <mirjam.cuper@gmail.com>
2022-05-28 12:36:45 +0200

    Altered lessen 8

--- _build/jupyter_execute/8_page_ET.py
+++ _build/jupyter_execute/8_page_ET.py
@@ -24,7 +24,7 @@
 # :class: attention
 # Import the ElemenTree package and load the XML file into your Notebook.  
 # You can look back to lesson 4 if you need a reminder on how to do this. 
-# The XML file is named 'page.xml' and can be downloaded here: ***insert link***.
+# The XML file is named 'page.xml' and can be [downloaded here](https://github.com/MirjamC/xml-workshop/tree/master/data).
 # ```
 # 
 # ````{admonition} Solution
